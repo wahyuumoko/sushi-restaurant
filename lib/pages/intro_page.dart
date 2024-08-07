931d3b300f0a1decdfb7cedbeb1919699eec723a
wahyuprihatmoko788 <wahyuprihatmoko788@gmail.com>
2024-08-07 01:48:07 +0000

    sushi

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 138, 60, 55),
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 25),
+
+            //shop name
+            Text(
+              "Sushi-Mania",
+              style: GoogleFonts.dmSerifDisplay(
+                fontSize: 28,
+                color: Colors.white,
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            //Icon
+            Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: Image.asset('lib/images/salmon_eggs.png'),
+            ),
+
+            const SizedBox(height: 25),
+
+            //title
+            Text(
+              "TASTE OF JAPANESE FOOD",
+              style: GoogleFonts.dmSerifDisplay(
+                fontSize: 44,
+                color: Colors.white,
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            //subtitle
+            Text("Feel the taste of the most popular Japanese food from anywhere and anytime"),
+
+            //get started button
+          ],
+        ),
+      ),
+    );
+  }
+}
