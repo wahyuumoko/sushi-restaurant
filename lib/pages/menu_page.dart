625616f7994aa5dadb775963a8be7a7fc5a8767d
wahyuprihatmoko788 <wahyuprihatmoko788@gmail.com>
2024-08-09 03:34:03 +0000

    warna

--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/components/button.dart';
+import 'package:myapp/theme/colors.dart';
 
 class MenuPage extends StatefulWidget {
   const MenuPage({super.key});
@@ -10,6 +12,51 @@ class MenuPage extends StatefulWidget {
 class _MenuPageState extends State<MenuPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Icon(
+          Icons.menu,
+          color: Colors.grey[900],
+        ),
+        title: Text(
+          'Tokyo',
+          style: TextStyle(color: Colors.grey[900]),
+        ),
+      ),
+      body: Column(
+        children: [
+          // promo banner
+          Container(
+            decoration: BoxDecoration(color: primaryColor),
+            child: Row(
+              children: [
+                Column(
+                  children: [
+                    // promo message
+                    Text('Get Promo 32% Off'),
+
+                    // redeem code
+                    MyButton(
+                      text: "Redeem",
+                      onTap: () {},
+                    ),
+                  ],
+                ),
+
+                //images
+              ],
+            ),
+          ),
+          //search bar
+
+          // menu list
+
+          // popular food
+        ],
+      ),
+    );
   }
 }
